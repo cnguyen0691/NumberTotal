e3f9afc1ff8ecb7f75cca05a81eb57b124f88d12
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-27 16:13:54 -0400

    average score

--- Main.java
+++ Main.java
@@ -14,6 +14,18 @@ public class Main {
         int w = x+y+z;
         System.out.println("Total: "+ w);
 
-
+        //Average Score
+        int score1 = input.nextInt();
+        int score2 = input.nextInt();
+        int score3 = input.nextInt();
+        int score4 = input.nextInt();
+        int score5 = input.nextInt();
+        int score6 = input.nextInt();
+        int score10 = input.nextInt();
+        int score7 = input.nextInt();
+        int score8 = input.nextInt();
+        int score9 = input.nextInt();
+        int AverageScore = (score1+score2+score3+score4+score5+score6+score7+score8+score9+score10)/10;
+        System.out.println("Average Score: "+ AverageScore);
     }
 }
